6a9c8540fc08a2524bb3cc0b37456ec65e98bd6a
wisnudj <jayalahwisnu@gmail.com>
2017-10-10 17:26:02 +0700

    buat routing dokter

--- routers/dokter.js
+++ routers/dokter.js
@@ -2,4 +2,19 @@ let express = require('express');
 let router = express.Router();
 let model = require('../models');
 
+router.get('/', (req, res) => {
+  model.Dokter.findAll().then((data) => {
+    res.render('dokter', {title: 'Daftar Dokter', data_DokterToEjs: data})
+  })
+})
+
+router.get('/add', (req, res) => {
+  res.render('dokteradd', {title: 'Tambah Dokter'})
+})
+
+//router.post('/add', (req, res) => {
+  //model.Dokter.create()
+//})
+
+
 module.exports = router;
