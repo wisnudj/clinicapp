475f950e370f2e6e83be159de5ca6dacc222b5ae
wisnudj <jayalahwisnu@gmail.com>
2017-10-11 00:57:18 +0700

    crud dokter

--- routers/dokter.js
+++ routers/dokter.js
@@ -12,9 +12,29 @@ router.get('/add', (req, res) => {
   res.render('dokteradd', {title: 'Tambah Dokter'})
 })
 
-//router.post('/add', (req, res) => {
-  //model.Dokter.create()
-//})
+router.post('/add', (req, res) => {
+  model.Dokter.create({name: req.body.name, spesialis: req.body.spesialis, jeniskelamin: req.body.jeniskelamin}).then(() => {
+    res.redirect('/dokter')
+  })
+})
+
+router.get('/edit/:id', (req, res) => {
+  model.Dokter.findOne({where: {id: req.params.id}}).then((data) => {
+    res.render('dokteredit', {title: 'Edit Dokter', data_DokterToEjs: data})
+  })
+})
+
+router.post('/edit/:id', (req, res) => {
+  model.Dokter.update({name: req.body.name, spesialis: req.body.spesialis, jeniskelamin: req.body.jeniskelamin}, {where:{id: req.params.id}}).then(() => {
+    res.redirect('/dokter')
+  })
+})
+
+router.get('/delete/:id', (req, res) => {
+  model.Dokter.destroy({where: {id:req.params.id}}).then(() => {
+    res.redirect('/dokter')
+  })
+})
 
 
 module.exports = router;
