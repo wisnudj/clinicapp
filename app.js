3f0f89c44bcc537ffa5cb6f519b844cded54b0f1
wisnudj <jayalahwisnu@gmail.com>
2017-10-10 16:33:14 +0700

    perubahan routing

--- app.js
+++ app.js
@@ -2,25 +2,32 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var session = require('express-session')
 var app = express()
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
+
 // parse application/json
 app.use(bodyParser.json())
+
+//ejs
 app.set('view engine', 'ejs');
+
+//css
 app.use(express.static(__dirname+'/views'))
 
-//var teacher = require('./routers/teacher');
+//require routers
 var dokter = require('./routers/dokter')
 var pasien = require('./routers/pasien')
 var diagnosis = require('./routers/diagnosis')
 var user = require('./routers/diagnosis')
 var index = require('./routers/index')
 
-//app.use('/index', index)
-app.use('./index', index)
+//route
+app.use('/', index)
 app.use('/dokter', dokter)
 app.use('/pasien', pasien)
 app.use('/diagnosis', diagnosis)
 app.use('/user', user)
 
+//listen
 app.listen(process.env.PORT || '3000')
--- routers/index.js
+++ routers/index.js
@@ -2,4 +2,5 @@ let express = require('express');
 let router = express.Router();
 let model = require('../models');
 
+
 module.exports = router;
