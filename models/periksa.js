23c412224ef81d49827cdfbb6dc93d441367b2fb
wisnudj <jayalahwisnu@gmail.com>
2017-10-11 18:48:31 +0700

    routing dan view periksa

--- models/periksa.js
+++ models/periksa.js
@@ -1,6 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Periksa = sequelize.define('Periksa', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     PasienId: DataTypes.INTEGER,
     DokterId: DataTypes.INTEGER,
     DiagnosisId: DataTypes.INTEGER,
--- routers/periksa.js
+++ routers/periksa.js
@@ -4,13 +4,17 @@ let model = require('../models')
 
 router.get('/', (req, res) => {
   model.Periksa.findAll({include: [model.Pasien, model.Dokter, model.Diagnosis]}).then((data) => {
-    res.send(data)
-    res.render('periksa', {title: 'Daftar Periksa Pasien', dataPeriksa_ToEjs: data})
+
+    var tanggal = data[0].tglperiksa.toDateString()
+    console.log(tanggal);
+    res.render('periksa', {title: 'Daftar Periksa Pasien', dataPeriksa_ToEjs: data, tanggal: tanggal})
   })
 })
 
 router.get('/add', (req, res) => {
-  res.render('periksaadd', {title: 'Add Periksa Pasien'})
+  model.Diagnosis.findAll().then((data) => {
+    res.render('periksaadd', {title: 'Add Periksa Pasien', dataDiagnosis: data})
+  })
 })
 
 router.post('/add', (req, res) => {
@@ -19,5 +23,24 @@ router.post('/add', (req, res) => {
   })
 })
 
+router.get('/delete/:id', (req, res) => {
+  model.Periksa.destroy({where: {id: req.params.id}}).then(() => {
+    res.redirect('/periksa')
+  })
+})
+
+router.get('/edit/:id', (req, res) => {
+  model.Periksa.findOne({where: {id: req.params.id}}).then((dataPeriksa) => {
+    model.Diagnosis.findAll().then((dataDiagnosis) => {
+      res.render('periksaedit', {title: "Edit Periksa", dataPeriksa_ToEjs: dataPeriksa, dataDiagnosis_To_Ejs: dataDiagnosis})
+    })
+  })
+})
+
+router.post('/edit/:id', (req, res) => {
+  model.Periksa.update({PasienId: req.body.PasienId, DokterId: req.body.DokterId, tglperiksa: req.body.tglperiksa, DiagnosisId: req.body.DiagnosisId, catatandokter: req.body.catatandokter}, {where: {id: req.params.id}}).then(() => {
+    res.redirect('/periksa')
+  })
+})
 
 module.exports = router
